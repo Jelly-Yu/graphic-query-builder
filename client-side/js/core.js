8e19f888dacccdc7fdeaf1c7efe5374adaac9692
Jelly-Yu <jelly.yu1992@gmail.com>
2016-02-26 14:22:25 -0500

    implemented UI and basic functionalities

---
+++ client-side/js/core.js
@@ -0,0 +1,256 @@
+/**
+ * Created by ct74048 on 1/7/2016.
+ */
+var cells = [];
+var tableFields = [];
+var tables = new Map();
+var allSelected = false;
+var tableStr = "",
+    fieldStr = "",
+    queryStatement = "",
+    serverURL = "",
+    ksName = "";
+
+$(document).ready(function() {
+
+    //create a floating query output area
+    $(window).scroll(function(){
+        $("#query_str").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
+    });
+
+
+    //get all tables in db.test_keyspace 
+    $.ajax({
+        type:"GET",
+        url: serverURL + 'tables',
+        success:function(data) {
+
+            console.log(data);
+            //var tbs = $.parseJSON(data);
+            for(var i =0; i< data.length;i++){
+                var name = data[i].columnfamily_name;
+                console.log(name);
+                tables.set(name, []);
+            }
+
+            //initialize table select
+            $('#field_select').searchableOptionList();
+            $('#table_select').searchableOptionList({
+                //format data for option list
+                data:function(){
+                    dataInSolFormat = [];
+                    $.each(data,function(i,v){
+                        var name = v.columnfamily_name;
+                        singleOption = {};
+                        singleOption["type"]="option";
+                        singleOption["label"]= name;
+                        singleOption["value"] = name;
+                        dataInSolFormat.push(singleOption);
+                    });
+                    return dataInSolFormat;
+                },
+                //register trigger events
+                events:{
+                    onChange:function(sol,changedElements){
+                        console.log(sol.getSelection());
+                        getColumns(sol.getSelection());
+
+                    }
+                },
+                maxHeight: '150px'
+            });
+        },
+
+        error: function( req, status, err ) {
+            console.log( 'something went wrong', status, err );
+            alert("500: Internal Server Error");
+
+        }
+    });
+
+    //bind click event to submit button
+    $("#btn_submit").click(function(){
+        console.log("hit the button");
+        //console.log(queryStatement);
+        var searchQuery = "";
+        console.log(":"+ queryStr+":");
+        searchQuery = queryStr.length > 5 ?  queryStatement + "WHERE " + queryStr : queryStatement;
+        fetchData(searchQuery);
+    });
+
+    //resizable columns
+    var pressed = false;
+    var start = undefined;
+    var startX, startWidth;
+
+    $("table").on('mousedown','th', function(e) {
+        start = $(this);
+        pressed = true;
+        startX = e.pageX;
+        startWidth = $(this).width();
+        $(start).addClass("resizing");
+        $(start).addClass("noSelect");
+    });
+
+    $(document).on('mousemove',function(e) {
+        if(pressed) {
+            $(start).width(startWidth+(e.pageX-startX));
+        }
+    });
+
+    $(document).on('mouseup',function() {
+        if(pressed) {
+            $(start).removeClass("resizing");
+            $(start).removeClass("noSelect");
+            pressed = false;
+        }
+    });
+});
+
+function getColumns(selectedItems){
+    if(selectedItems.length < 1){
+        renderOptionList(selectedItems);
+    }else{
+        $.each(selectedItems,function(i,v){
+            var tablename = v.value;
+            if(tables.get(tablename).length > 0){
+                renderOptionList(selectedItems);
+            }else{
+                $.ajax({
+                    type:"GET",
+                    url:serverURL + 'metadata/description?table='+tablename,
+                    success:function(data){
+                        console.log(data);
+                    },
+                    error: function(req, status, err){
+                        console.log('something bad happened', status, err);
+                    }
+                });
+                $.ajax({
+                    type:"GET",
+                    url:serverURL + 'tables/columns?table='+tablename,
+                    success:function(data) {
+                        console.log(data);
+                        var cols = data;
+                        var temp = tables.get(tablename);
+                        for(var i =0; i< cols.length;i++){
+                            var name = cols[i].column_name;
+                            console.log(name);
+                            temp.push(name);
+                            console.log(temp);
+                        }
+                        tables.set(tables.get(tablename), temp);
+                        console.log(tables.get(tablename));
+                        renderOptionList(selectedItems);
+                    },
+                    error: function( req, status, err ) {
+                        console.log( 'something went wrong', status, err );
+                    }
+                });
+            }            
+        });        
+    }    
+}
+
+function renderOptionList(selectedItems){
+    //update options in field select according to selected options in table select
+    $('#field_selector').empty();
+    $('#field_selector').append('<select id="field_select" name="character" multiple="multiple"></select>');
+    $('#field_select').searchableOptionList({
+        data:function(){
+            dataInSolFormat = [];
+            tableStr = "";
+            $.each(selectedItems,function(i,v){
+                var name = v.value;
+                tableStr = tableStr+ name + ',';
+                if(tables.has(name)){
+                    tname = tables.get(name);
+                    console.log(tname);
+                    $.each(tname, function(i, item){
+                        singleOption = {};
+                        singleOption["type"]="option";
+                        singleOption["label"]= item + " ("+name+")";
+                        singleOption["value"] = item;
+                        dataInSolFormat.push(singleOption);
+                    });                                        
+                }
+                else{
+                    console.log("table not exist");
+                }            
+            });
+            console.log(tableStr);
+            fieldStr="";
+            var temp = allSelected === true? "*" : fieldStr.substring(0,fieldStr.length -1);
+            var str = "SELECT "+ temp + " FROM "+ ksName + tableStr.substring(0, tableStr.length-1) ;
+            $("#result").html(str);
+            queryStatement = str+ " ";
+            return dataInSolFormat; 
+        },
+        events:{
+            onChange:function(sol,changedElements){
+                console.log(sol.getSelection());
+                tableFields= [];
+                fieldStr="";
+                $.each(sol.getSelection(),function(i,item){
+                    fieldStr = fieldStr +item.value + ',';
+                    newField = {};
+                    newField["name"] = item.value;
+                    tableFields.push(newField);
+                });
+                console.log(fieldStr);
+                var temp = allSelected === true? "*" : fieldStr.substring(0,fieldStr.length -1);
+                var str = "SELECT "+ temp + " FROM "+ ksName + tableStr.substring(0, tableStr.length-1);
+                $("#result").html(str);
+                queryStatement = str+ " ";
+            }
+        },
+        maxHeight:250
+    });
+}
+//send query statement to server via post request 
+function fetchData(query){
+    
+    console.log(query);    
+    $.post(serverURL + "search", {qStr: query},function(data){
+
+    }).done(function(data){
+        console.log(data);
+        cells = data.slice();
+        //console.log(items);
+        populateTable();
+    })
+    .fail(function(){
+        $("#result_info").html("<p>Failed to parse the query.</p>");
+        $("#table_header").html("");
+        $("#table_rows").html("");
+
+    });
+}
+
+function populateTable(){
+    $("#result_info").html("");
+    var firstItem = cells[0];
+    var content = "";
+    var temp_keys = [];
+    for(var key in firstItem){
+        console.log(key);
+        if(firstItem.hasOwnProperty(key)){
+            content += "<th><div class='noCrsr'>"+key+"</div></th>" ;
+            temp_keys.push(key);
+        }
+    }
+   // console.log(keys);
+    $("#table_header").html(content);
+    content = "<tr>";
+    $.each(cells, function(i,c){
+        //console.log(item);
+        for(var i = 0; i< temp_keys.length;i++){
+
+            var val = c[temp_keys[i]];
+            console.log(val);
+            content += "<td>"+val+"</td>";
+        }
+        content += "</tr>";
+    })
+    $("#table_rows").html(content);
+}
---
+++ client-side/js/query_builder/query-builder.js
@@ -0,0 +1,115 @@
+/**
+ * Created by ct74048 on 1/14/2016.
+ */
+var queryStr = "";
+var app = angular.module('app_search', ['ngSanitize', 'queryBuilder']);
+app.controller('QueryBuilderCtrl', ['$scope', function ($scope) {
+    var data = '{"group": {"operator": "AND","rules": []}}';
+    function htmlEntities(str) {
+        return String(str).replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    }
+
+    function computed(group) {
+        if (!group) {
+            return "";
+        }
+        for (var str = "", i = 0; i < group.rules.length; i++) {
+            i > 0 && (str += " <strong>" + group.operator + "</strong> ");
+            str += group.rules[i].group ?
+                computed(group.rules[i].group) :
+            group.rules[i].field + " " + htmlEntities(group.rules[i].condition) + " " + group.rules[i].data;
+        }
+
+        return str+" ";
+    }
+
+    $scope.json = null;
+
+    $scope.filter = JSON.parse(data);
+
+    $scope.$watch('filter', function (newValue) {
+        $scope.json = JSON.stringify(newValue, null, 2);
+        $scope.output = computed(newValue.group);
+        queryStr = $scope.output+" ";
+        console.log(queryStr);
+    }, true);
+}]);
+
+var queryBuilder = angular.module('queryBuilder', []);
+queryBuilder.directive('queryBuilder', ['$compile', function ($compile) {
+    return {
+        restrict: 'E',
+        scope: {
+            group: '='
+        },
+        templateUrl: '/queryBuilderDirective.html',
+        compile: function (element, attrs) {
+            var content, directive;
+            content = element.contents().remove();
+            return function (scope, element, attrs) {
+                scope.operators = [
+                    { name: 'AND' },
+                    { name: 'OR' }
+                ];
+
+                console.log("xxx:"+tableFields.slice());
+               scope.fields = tableFields.slice();
+
+                console.log(scope.fields);
+
+                scope.conditions = [
+                    { name: '=' },
+                    { name: '<' },
+                    { name: '<=' },
+                    { name: '>' },
+                    { name: '>=' },
+                    { name: 'IN'}
+                ];
+
+                scope.addCondition = function () {
+                    console.log("add condition is hit");
+                    if(typeof tableFields.slice()[0] === 'undefined'){
+                        console.log("data is not available currently");
+                    }else{
+                        item = {};
+                        item["condition"] = "=";
+                        item["field"] = String(tableFields.slice()[0].name);
+                        console.log(tableFields.slice()[0].name);
+                        item["data"] = '';
+                        scope.group.rules.push(item);
+                    }
+
+                };
+
+                scope.removeCondition = function (index) {
+                    console.log("remove condition is hit");
+                    scope.group.rules.splice(index, 1);
+                };
+                scope.update = function(){
+                    scope.fields = tableFields.slice();
+                    console.log(scope.fields);
+                };
+
+                scope.addGroup = function () {
+                    scope.group.rules.push({
+                        group: {
+                            operator: 'AND',
+                            rules: []
+                        }
+                    });
+                };
+
+                scope.removeGroup = function () {
+                    console.log("remove group is hit");
+                    "group" in scope.$parent && scope.$parent.group.rules.splice(scope.$parent.$index, 1);
+                };
+
+                directive || (directive = $compile(content));
+
+                element.append(directive(scope, function ($compile) {
+                    return $compile;
+                }));
+            }
+        }
+    }
+}]);
